e83a774adff2d296b4271e6bae90f9dd3e603285
Benson Mokogi <b.mokogi@gmail.com>
2023-03-31 00:11:38 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for(i=0; i<array.length; i++){
 
+    for (j=1; j<array.length; j++){
+
+      if (array[i] + array[j] === target){
+
+        console.log(`${array[i]},${array[j]}`)
+
+        return true;
+      } 
+    }
+  }
+
+}
+hasTargetSum([3, 8, 12, 4, 11, 7], 10);
 /* 
   Write the Big O time complexity of your function here
+
+  1. Both first and second iteration have n steps
+  2. O(n²) => quadratic time
 */
 
 /* 
   Add your pseudocode here
+
+  1. Iterate through the array in two step.
+  2. Checking if an elements from the first and second iteration whose sum equal the target parameter.
+  3. Return true and log the two element , else return false.
 */
 
 /*
